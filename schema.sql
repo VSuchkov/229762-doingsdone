a0c548e84718a8318e8430296119d2a35dfeaa47
Vladimir Suchkov <ssvovs@mail.ru>
2017-05-16 18:39:22 +0300

    исправление замечаний schema

--- schema.sql
+++ schema.sql
@@ -2,29 +2,29 @@ CREATE DATABASE doingsdone;
 
 USE doingsdone;
 
-  CREATE TABLE categories (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name CHAR(128),
-  user_id CHAR(128)
+CREATE TABLE projects (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name CHAR(128),
+user_id CHAR(128)
 );
 
 CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  email CHAR(128),
-  login CHAR(128),
-  password CHAR(32),
-  avatar_path CHAR(255)
+id INT AUTO_INCREMENT PRIMARY KEY,
+email CHAR(128),
+login CHAR(128),
+password CHAR(128),
+avatar_path CHAR(255)
 );
 
 CREATE TABLE tasks (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  category_id INT,
-  user_id INT
-  task CHAR;
-  date_done DATETIME;
-  category INT;
-  done INT;
-  path CHAR(255)
+id INT AUTO_INCREMENT PRIMARY KEY,
+project_id INT,
+user_id INT,
+task CHAR,
+date_done DATETIME,
+project INT,
+done INT,
+path CHAR(255)
 );
 
 CREATE INDEX task ON tasks(task);
