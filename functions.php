6c6fa4f8d8ad1133ab763b77b9fb973a3de0a036
Vladimir Suchkov <ssvovs@mail.ru>
2017-05-22 23:54:48 +0300

    устраняю замечания

--- functions.php
+++ functions.php
@@ -33,33 +33,35 @@ function searchUserByEmail($email, $users) {
      }
      return $result;
 }
-
+/*
 $con = mysqli_connect("localhost", "root", "", "doingsdone");
 $name = $_POST['name'];
 $password = $_POST['password'];
 $sql = 'SELECT users ('name', 'password') VALUES (?, ?)';
+*/
 function get_data($con, $sql, $data) {
     $data_array = [];
-    $result = db_get_prepare_stmt ($con, $sql, $data);
+    $result = db_get_prepare_stmt($con, $sql, $data);
     if ($result == false) {
-        return [];
+        return $data_array;
     } else {
         mysqli_stmt_execute($result);
-        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC) {
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
         $data_array[] = $row;
-        return $data_array;
         }
+    return $data_array;
     }
 }
 
 function include_data($con, $sql, $data) {
     $result = db_get_prepare_stmt($con, $sql, $data);
-    mysqli_stmt_execute($result);
-    $last_id = mysql_insert_id($con);
     if ($result == false) {
         return false;
     } else {
+        mysqli_stmt_execute($result);
+        $last_id = mysql_insert_id($con);
         return $last_id;
+
     }
 }
 
@@ -67,6 +69,14 @@ function update_data($con, $table_name, $update_data, $update_condition) {
     $update_string = "";
     $condition_string = "";
 
+    $data_keys = array_keys($update_data);
+    $update_string = implode(" = ?,", $data_keys);
+    $update_string .= " = ? ";
+
+    $condition_keys = array_keys($update_condition);
+    $condition_string = implode(" = ?,", $condition_keys);
+    $condition_string .= " = ? ";
+/*
     foreach ($update_data as $key => $value) {
         $update_string .= "$key = ?,";
     }
@@ -74,15 +84,14 @@ function update_data($con, $table_name, $update_data, $update_condition) {
     foreach ($update_condition as $key => $value) {
         $condition_string .= "$key = ?,";
     }
+*/
     $sql = "UPDATE $table_name SET $update_string WHERE $condition_string";
     $merge_update = array_merge($update_data, $update_condition);
     $result = db_get_prepare_stmt($con, $sql, $merge_update);
-    mysqli_stmt_execute($result);
-    $records_count = mysqli_num_rows($result);
-
     if ($result == false) {
         return false;
     } else {
+        $records_count = mysqli_num_rows($result);
         return $records_count;
     }
 }
